6b69b94f5359c708665578788fe7d181ec2fefbd
Armando Santos <armandoifsantos@gmail.com>
2021-01-26 21:57:13 +0000

    Fixed font issue with doom-emacs

--- home/programs/emacs/doom.d/config.el
+++ home/programs/emacs/doom.d/config.el
@@ -19,7 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :weight 'normal))
+;; JetBrainsMono Nerd Font Mono
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :weight 'normal))
 ;; doom-variable-pitch-font (font-spec :family "JetBrains" :size 15)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
